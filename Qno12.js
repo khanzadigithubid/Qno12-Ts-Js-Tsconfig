887423c24810cf9d8dc2da746f889cf5fb03bec4
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:36:04 +0500

    Qno12 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno12 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno12.js
@@ -0,0 +1,10 @@
+"use strict";
+/*Greetings: Start with the array you used in Exercise 11, but instead of just printing
+ each person's name, print a message to them. The text of each message should be the same,
+but each meassage should be personalized with the person's name.*/
+let names = ["Khanzadi", "Sapna", "Ayesha", "Kiran"];
+let message = "How are your Dear?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
---
+++ Qno12.ts
@@ -0,0 +1,10 @@
+/*Greetings: Start with the array you used in Exercise 11, but instead of just printing
+ each person's name, print a message to them. The text of each message should be the same,
+but each meassage should be personalized with the person's name.*/
+
+let names: string[] = ["Khanzadi", "Sapna", "Ayesha", "Kiran"];
+let message: string = "How are your Dear?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
